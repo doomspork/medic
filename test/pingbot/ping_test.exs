02d4f1deb1b962524207b118b525dd2a4267acf6
Sean Callan <seancallan@gmail.com>
2015-09-14 15:16:55 -0700

    Implement basic ping functionality

---
+++ test/pingbot/ping_test.exs
@@ -0,0 +1,18 @@
+defmodule Pingbot.PingTest do
+  use ExUnit.Case
+
+  alias Pingbot.Ping
+
+  test "reports successful pings" do
+    Ping.ping("127.0.0.1", self)
+
+    assert_receive {:ok, "127.0.0.1", time}
+    IO.inspect time
+    assert is_float(time)
+  end
+
+  test "reports failed pings" do
+    Ping.ping("127.0.0.0", self)
+    assert_receive {:ok, "127.0.0.0", false}
+  end
+end

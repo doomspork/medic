23d34f7e8cb426a6c7eaa4b63986e9e8fcd8482e
Sean Callan <seancallan@gmail.com>
2015-10-15 16:48:37 -0700

    Re-introduce transport types for testing

--- lib/medic/reporter.ex
+++ lib/medic/reporter.ex
@@ -6,7 +6,49 @@ defmodule Medic.Reporter do
 
   require Logger
 
-  @auth_token Application.get_env(:medic, :auth_token)
+  @defaults [transport: JSON]
+
+  defmodule JSON do
+    @moduledoc """
+    Report the results via an JSON API request
+    """
+
+    @doc """
+    Transmit report as JSON via HTTP to destination
+    """
+    def transmit(dest, report) do
+      %{"report" => report}
+      |> Poison.encode!
+      |> http_post(dest)
+    end
+
+    @doc false
+    defp auth_token, do: Application.get_env(:medic, :auth_token)
+
+    @doc false
+    defp http_post(report, dest) do
+      HTTPoison.post!(dest, report, http_headers)
+    end
+
+    @doc false
+    defp http_headers do
+      [
+        {"Content-Type", "application/json"},
+        {"Authorization", "Bearer " <> auth_token}
+      ]
+    end
+  end
+
+  defmodule Inbox do
+    @moduledoc """
+    Using the OTP mailbox to submit report.
+    """
+
+    @doc """
+    Use Kernel.send to transmit report
+    """
+    def transmit(dest, report), do: send(dest, report)
+  end
 
   ### Client API
 
@@ -20,7 +62,7 @@ defmodule Medic.Reporter do
   @doc """
   Initialize our GenServer.
   """
-  def init(opts), do: {:ok, opts}
+  def init(opts), do: {:ok, Keyword.merge(@defaults, opts)}
 
   @doc """
   Submit a given report.
@@ -31,27 +73,7 @@ defmodule Medic.Reporter do
 
   @doc false
   def handle_cast(report, opts) do
-    %{"report" => report}
-    |> Poison.encode!
-    |> http_post(opts[:dest])
-
+    Task.start(opts[:transport], :transmit, [opts[:dest], report])
     {:noreply, opts}
   end
-
-  @doc false
-  defp auth_token, do: Application.get_env(:medic, :auth_token)
-
-  @doc false
-  defp http_post(report, dest) do
-    HTTPoison.post!(dest, report, http_headers)
-  end
-
-  @doc false
-  defp http_headers do
-    [
-      {"Content-Type", "application/json"},
-      {"Authorization", "Bearer " <> auth_token}
-    ]
-  end
-
 end
